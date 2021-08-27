9fc4a13bb4b6f290e913e14e4d20c6d6fb0062b4
Rickard <rickardp@users.noreply.github.com>
2021-08-27 08:41:30 +0000

    Fixed null error

--- src/Combination.DynamicCors/DynamicCorsMiddleware.cs
+++ src/Combination.DynamicCors/DynamicCorsMiddleware.cs
@@ -12,9 +12,9 @@ namespace Combination.DynamicCors
     {
         private readonly Regex allowedHosts;
         private readonly string methods;
-        private readonly ILogger logger;
+        private readonly ILogger? logger;
 
-        public DynamicCorsMiddleware(Regex allowedHosts, string methods, ILogger<DynamicCorsMiddleware> logger)
+        public DynamicCorsMiddleware(Regex allowedHosts, string methods, ILogger<DynamicCorsMiddleware>? logger)
         {
             this.allowedHosts = allowedHosts;
             this.methods = methods;
@@ -28,28 +28,28 @@ namespace Combination.DynamicCors
             var origin = string.Empty;
             var isReferer = false;
 
-            if (context.Request.Headers.TryGetValue("Origin", out var value))
+            if (context.Request.Headers.TryGetValue("Origin", out var value)
+                && value.FirstOrDefault() is string originValue)
             {
-                var valueStr = value.FirstOrDefault();
-                if (!string.Equals(valueStr, "null", StringComparison.OrdinalIgnoreCase))
+                if (!string.Equals(originValue, "null", StringComparison.OrdinalIgnoreCase))
                 {
-                    origin = valueStr;
+                    origin = originValue;
                 }
             }
 
             // Origin will be "null" for redirects
-            if (string.IsNullOrEmpty(origin) && context.Request.Headers.TryGetValue("Referer", out var referer))
+            if (string.IsNullOrEmpty(origin) && context.Request.Headers.TryGetValue("Referer", out var referer)
+                && referer.FirstOrDefault() is string refererValue)
             {
-                var valueStr = referer.FirstOrDefault();
-                var index = valueStr.IndexOf("//", StringComparison.InvariantCultureIgnoreCase) + 2;
-                index = valueStr.IndexOf('/', index);
+                var index = refererValue.IndexOf("//", StringComparison.InvariantCultureIgnoreCase) + 2;
+                index = refererValue.IndexOf('/', index);
 
                 if (index > 0)
                 {
-                    valueStr = valueStr.Substring(0, index);
+                    refererValue = refererValue.Substring(0, index);
                 }
 
-                origin = valueStr;
+                origin = refererValue;
                 isReferer = true;
             }
 
@@ -63,7 +63,7 @@ namespace Combination.DynamicCors
             }
             else if (!string.IsNullOrEmpty(origin))
             {
-                logger.LogWarning("CORS validation failed for origin: {0}. Methods: {1}. Hosts: {2}", origin, methods, allowedHosts.ToString());
+                logger?.LogWarning("CORS validation failed for origin: {0}. Methods: {1}. Hosts: {2}", origin, methods, allowedHosts.ToString());
             }
 
             if (context.Request.Method == HttpMethods.Options)
