21b09757739c0b2f0d8619dfaacb23c2f12a1cec
Rickard <rickardp@users.noreply.github.com>
2021-08-30 13:42:59 +0200

    Update IDynamicCorsFactory.cs

--- src/Combination.DynamicCors/IDynamicCorsFactory.cs
+++ src/Combination.DynamicCors/IDynamicCorsFactory.cs
@@ -4,6 +4,7 @@ namespace Combination.DynamicCors
 {
     public interface IDynamicCorsFactory
     {
+        //Test
         IDynamicCorsFactory WithMethods(params string[] methods);
 
         IDynamicCorsFactory WithPattern(Regex regex);
